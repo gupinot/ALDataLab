2111397c1a47ca1171387b2871fe28aa36a7bf00
Guillaume Pinot <guillaume.pinot.fr@gmail.com>
2015-11-24 11:36:47 +0100

    Without hadoop link without step 3to4

--- src/main/scala/DLRepo.scala
+++ src/main/scala/DLRepo.scala
@@ -1,6 +1,7 @@
 package DLRepo
 
 import dlenv._
+import dlutil._
 
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs._
@@ -8,6 +9,7 @@ import org.apache.hadoop.fs.Path
 import org.apache.commons.io.FilenameUtils
 import com.databricks.spark.csv._
 import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions.lit
 import org.apache.spark.{SparkConf, SparkContext}
 
 
@@ -17,8 +19,12 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 class dlrepo(RepoDir: String) {
 
-  def ProcessInFile(sqlContext: org.apache.spark.sql.SQLContext): Boolean = {
+  def ProcessInFile(sqlContext: org.apache.spark.sql.SQLContext, filein: String): Boolean = {
     //Read csv files from /DATA/Repository/in and convert to avro format
+
+    import sqlContext.implicits._
+
+    /*
 
     val hadoopConfig = new hadoop_env
     val fs = hadoopConfig.init()
@@ -27,22 +33,67 @@ class dlrepo(RepoDir: String) {
       println("Create " + RepoDir + "/Done")
       fs.mkdirs(new Path(RepoDir + "/Done"))
     }
+    */
+    //val filelist = fs.globStatus(new Path(RepoDir + "/in/*.csv"))
 
-    val filelist = fs.globStatus(new Path(RepoDir + "/in/*.csv"))
 
-    filelist.foreach(fileStatus => {
-      val filename = fileStatus.getPath().toString()
-      println("filename = " + filename)
-      val parquetfilename = RepoDir + "/" + FilenameUtils.getBaseName(filename) + ".parquet"
-      println("parquetfilename = " + parquetfilename)
+
+    //filelist.foreach(fileStatus => {
+      val filename = new Path(filein).getName()
+      val filetype = filename.replaceFirst("_.*", "")
+      val filedate = filename.replaceFirst(filetype+"_", "").replaceFirst("\\.csv", "")
+
+      println("ProcessInFile() : filename=" + filein + ", filetype=" + filetype + ", filedate=" + filedate)
+      val respath = RepoDir + "/" + filetype
+      println("ProcessInFile() : respath = " + respath )
       val res = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("delimiter", ";")
         .option("inferSchema", "true")
-        .load(filename)
-      res.write.format("parquet").mode("overwrite").save(parquetfilename)
-      println("load.write done")
-      fs.rename(new Path(filename), new Path(RepoDir + "/Done/" + FilenameUtils.getName(filename)))
-      println("rename done : " + filename + " ->" + RepoDir + "/Done/" + FilenameUtils.getName(filename))
-    })
+        .load(filein)
+        .withColumn("filedate", lit(filedate))
+
+      val res2 = {
+        filetype match {
+          case "MDM-ITC" => res
+            .select("Location Code", "IP Address", "Mask", "filedate")
+            .withColumnRenamed("Location Code", "mdm_loc_code")
+            .withColumnRenamed("IP Address", "mdm_ip_start")
+            .withColumnRenamed("Mask", "mdm_ip_range")
+          case "I-ID" => res
+          case "AIP-Server" => res
+            .select("Host name", "Function", "Type", "Sub-Function", "IP address", "Status", "Administrated by", "OS Name", "filedate")
+            .withColumnRenamed("Host name", "AIP_Server_HostName")
+            .withColumnRenamed("Function", "AIP_Server_Function")
+            .withColumnRenamed("Type", "AIP_Server_Type")
+            .withColumnRenamed("Sub-Function", "AIP_Server_SubFunction")
+            .withColumnRenamed("IP address", "AIP_Server_IP")
+            .withColumnRenamed("Status", "AIP_Server_Status")
+            .withColumnRenamed("Administrated by", "AIP_Server_AdminBy")
+            .withColumnRenamed("OS Name", "AIP_Server_OSName")
+          case "AIP-Application" => res
+            .select("Application Name", "Shared Unique ID", "Type", "Current State", "Sensitive Application", "Criticality", "Sector", "filedate")
+            .withColumnRenamed("Application Name", "aip_app_name")
+            .withColumnRenamed("Shared Unique ID", "aip_app_shared_unique_id")
+            .withColumnRenamed("Type", "aip_app_type")
+            .withColumnRenamed("Current State", "aip_app_state")
+            .withColumnRenamed("Sensitive Application", "aip_app_sensitive")
+            .withColumnRenamed("Criticality", "aip_app_criticality")
+            .withColumnRenamed("Sector", "aip_app_sector")
+          case "AIP-SoftInstance" => res
+            .select("Application Name", "Shared Unique ID", "Type", "Host name", "IP address", "filedate")
+            .withColumnRenamed("Application Name", "aip_appinstance_name")
+            .withColumnRenamed("Shared Unique ID", "aip_appinstance_shared_unique_id")
+            .withColumnRenamed("Type", "aip_appinstance_type")
+            .withColumnRenamed("Host name", "aip_appinstance_hostname")
+            .withColumnRenamed("IP address", "aip_appinstance_ip")
+          case "AIP-Flow" => res
+        }
+      }
+
+      res2.write.mode("append").partitionBy("filedate").parquet(respath)
+      println("ProcessInFile() : load.write done")
+      //fs.rename(new Path(filename), new Path(RepoDir + "/Done/" + FilenameUtils.getName(filename)))
+      //println("rename done : " + filename + " ->" + RepoDir + "/Done/" + FilenameUtils.getName(filename))
+
     return true
   }
 
@@ -93,8 +144,17 @@ class dlrepo(RepoDir: String) {
   }
 
   def readMDM(MDMRepository: String, sqlContext: org.apache.spark.sql.SQLContext): DataFrame = {
+    import org.apache.spark.sql.functions._
+    import sqlContext.implicits._
 
     val df = sqlContext.read.parquet(MDMRepository)
+      .filter(regexudf(iprangepattern)($"mdm_ip_range"))
+      .withColumn("mdm_ip_start", regexp_replace($"mdm_ip_start", "\\/.*", ""))
+      .withColumn("mdm_ip_start", regexp_replace($"mdm_ip_start", " ", ""))
+      .withColumn("mdm_ip_range", regexp_replace($"mdm_ip_range", " ", ""))
+      .withColumn("mdm_ip_start_int", ip2Long($"mdm_ip_start"))
+      .withColumn("mdm_ip_end_int", rangeToIP($"mdm_ip_start_int", $"mdm_ip_range"))
+
     return df
   }
 
