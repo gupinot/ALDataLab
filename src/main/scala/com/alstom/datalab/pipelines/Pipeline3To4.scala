ed6a0f609bfb0b6e08b49ce99a3faae154b47d66
Raphael Luta <raphael.luta@aptiwan.com>
2015-12-10 17:19:47 +0100

    * rename sme dataframes to shorten queries * implement a filter on read cnx and webrequets

--- src/main/scala/com/alstom/datalab/pipelines/Pipeline3To4.scala
+++ src/main/scala/com/alstom/datalab/pipelines/Pipeline3To4.scala
@@ -12,7 +12,6 @@ import org.apache.spark.sql.functions._
 /**
   * Created by guillaumepinot on 05/11/2015.
   */
-
 object Pipeline3To4 {
   val STAGE_NAME = "pipe3to4"
 }
@@ -27,101 +26,79 @@ class Pipeline3To4(sqlContext: SQLContext) extends Pipeline {
     val jobidcur:Long = System.currentTimeMillis/1000
     val sc = sqlContext.sparkContext
 
+    // main dataframes
+    val cnx_parquet = sqlContext.read.option("mergeSchema", "false").parquet(s"$dirin/connection/")
+    val wr = sqlContext.read.option("mergeSchema", "false").parquet(s"$dirin/webrequest/")
+
+    // metadata view on main dataframes
+    val cnx_meta = cnx_parquet.select($"collecttype" as "type",$"engine",$"dt",$"filedt")
+    val wr_meta = wr.select($"collecttype" as "type",$"engine",$"dt",$"filedt")
+
     //read control files from inputFiles and filter on connection filetype)
-    val dfControl: DataFrame = this.inputFiles.map(filein => {sc.textFile(filein)})
+    val control: DataFrame = this.inputFiles.map(filein => {sc.textFile(filein)})
       .reduce(_.union(_))
       .map(_.split(";"))
       .map(s => ControlFile(s(0), s(1), s(2), s(3), s(4), s(5), s(6), s(7)))
       .toDF()
-    val jobidorig=dfControl.select("jobid").distinct().head.getString(0)
 
-    val dfControlConnection = dfControl
+    val jobidorig=control.select("jobid").distinct().head.getString(0)
+
+    val ctl_cnx_only = control
       .filter($"filetype" === "connection")
-      .select($"collecttype" as "ctl_collecttype",
-        $"engine" as "ctl_engine",
-        to_date($"day") as "ctl_day",
-        to_date($"filedt") as "ctl_filedt")
+      .select($"collecttype" as "type", $"engine" as "engine", to_date($"day") as "dt", to_date($"filedt") as "filedt")
 
-    val dfdirinconnection = sqlContext.read.option("mergeSchema", "false").parquet(s"$dirin/connection/")
-    dfdirinconnection.persist(StorageLevel.MEMORY_AND_DISK)
-
-    //select correct filedt from dfdriin
-    val dfdirinconnectionCorrect = dfdirinconnection
-      //.select("filedt","collecttype","dt","engine")
-      .groupBy("collecttype", "dt", "engine")
-      .agg(min(to_date($"filedt")) as "minfiledt")
+    //select correct filedt from cn-onnections
+    val cnx_best = cnx_meta.groupBy("collecttype", "dt", "engine").agg(min(to_date($"filedt")) as "filedt")
 
     //select correct filedt from dfControlConnection
-    val dfControlConnectionToDo = dfdirinconnectionCorrect.join(dfControlConnection,
-      dfdirinconnectionCorrect("collecttype") === dfControlConnection("ctl_collecttype")
-        && dfdirinconnectionCorrect("engine") === dfControlConnection("ctl_engine")
-        && dfdirinconnectionCorrect("dt") === dfControlConnection("ctl_day")
-        && dfdirinconnectionCorrect("minfiledt") === dfControlConnection("ctl_filedt"),
-      "inner")
+    val cnx_all = cnx_best.as("cnx_best")
+      .join(ctl_cnx_only.as("ctl"),
+      ($"cnx_best.type" === $"ctl.type") && ($"cnx_best.engine" === $"ctl.engine")
+        && ($"cnx_best.dt" === $"ctl.dt") && ($"cnx_best.filedt" === $"ctl.filedt"), "inner").as("cnx")
+      .join(wr_meta.as("wr"),
+      ($"cnx.type" === $"wr.type") && ($"cnx.engine" === $"wr.engine")
+        && ($"cnx.filedt" === $"wr.filedt") && ($"cnx.dt" === $"wr.dt"), "left_outer")
 
-    dfControlConnectionToDo.persist(StorageLevel.MEMORY_AND_DISK)
-
-    // load corresponding webrequest
-
-    val dfwr = sqlContext.read.option("mergeSchema", "false").parquet(s"$dirin/webrequest/")
-    dfwr.persist(StorageLevel.MEMORY_AND_DISK).registerTempTable("webrequest")
-
-    //verify existence of all webrequest files corresponding to connection files in dfControlConnectionToDo
-    val dfwrRenammed = dfwr
-      .select("collecttype", "engine", "filedt", "dt")
-      .withColumnRenamed("collecttype", "wrcollecttype")
-      .withColumnRenamed("engine", "wrengine")
-      .withColumnRenamed("filedt", "wrfiledt")
-      .withColumnRenamed("dt", "wrdt")
-
-    val dfControlConnectionToDOWithWR = dfControlConnectionToDo.join(dfwrRenammed,
-      dfControlConnectionToDo("collecttype") === dfwrRenammed("wrcollecttype")
-        && dfControlConnectionToDo("engine") === dfwrRenammed("wrengine")
-        && dfControlConnectionToDo("minfiledt") === dfwrRenammed("wrfiledt")
-        && dfControlConnectionToDo("dt") === dfwrRenammed("wrdt"),
-      "left_outer")
-    val dfwrnotexists =  dfControlConnectionToDOWithWR.filter($"wrfiledt" === null)
-    val dfControlConnectionToDoFinal = {
-      if (dfwrnotexists .count() != 0) {
+    // save incomplete connections data to reject file
+    val cnx_no_wr = cnx_all.filter($"wr.filedt" === null)
+    if (cnx_no_wr .count() != 0) {
         println("following webrequest files not found :")
-        dfwrnotexists.select("collecttype", "engine", "minfiledt", "dt").withColumn("Status", lit("webrequest not found")).write.format("com.databricks.spark.csv")
+        cnx_no_wr.select($"type", $"engine", $"filedt", $"dt",lit("webrequest not found") as "Status")
+          .write.format("com.databricks.spark.csv")
           .option("header", "true")
           .option("delimiter", ";").save(direrr)
-        dfControlConnectionToDOWithWR.filter($"wrfiledt" !== null)
-      }else {
-        dfControlConnectionToDOWithWR
-      }
     }
 
+    // process all other lines
+    val cnx = cnx_all.filter($"wr.filedt" !== null)
+    val all_dt = cnx.select($"ctl.dt").distinct().collectAsList()
+
     //Read and resolve
-    val dfdirinconnection2 = dfdirinconnection
-      .join(dfControlConnectionToDoFinal, dfdirinconnection("collecttype") === dfControlConnectionToDoFinal("collecttype")
-        && dfdirinconnection("engine") === dfControlConnectionToDoFinal("engine")
-        && dfdirinconnection("filedt") === dfControlConnectionToDoFinal("minfiledt")
-        && dfdirinconnection("dt") === dfControlConnectionToDoFinal("dt"),
-        "inner")
-      .select(dfdirinconnection.columns.map(dfdirinconnection.col):_*)
+    val cnx_filtered = cnx_parquet.filter($"dt" isin all_dt)
+      .join(cnx, cnx_parquet("collecttype") === cnx("type") && cnx_parquet("engine") === cnx("engine")
+        && cnx_parquet("filedt") === cnx("minfiledt") && cnx_parquet("dt") === cnx("dt"), "inner")
+      .select(cnx_parquet.columns.map(cnx_parquet.col):_*)
 
-    println("pipeline3to4() : SiteResolution")
-    val dfSite = SiteResolutionFromIP(dfdirinconnection2)
+    val wr_filtered = wr.filter($"dt" isin all_dt)
+      .join(cnx, wr("collecttype") === cnx("type") && wr("engine") === cnx("engine")
+        && wr("filedt") === cnx("minfiledt") && wr("dt") === cnx("dt"), "inner")
+      .select(wr.columns.map(cnx_parquet.col):_*)
 
-    println("pipeline3to4() : Sector resolution")
-    val dfSiteSector = SiteAndSectorResolutionFromI_ID(dfSite)
+    println("pipeline3to4() : data resolutions")
+    val cnx_resolved = resolveAIP(resolveSector(resolveSite(cnx_filtered)))
 
-    //AIP resolution
-    println("pipeline3to4() : AIP resolution")
-    val dfResolved = AIPResolution(dfSiteSector)
-    dfResolved.registerTempTable("connections")
+    wr_filtered.registerTempTable("webrequests")
+    cnx_resolved.registerTempTable("connections")
 
     //join with df
-    val dfres = sqlContext.sql(
-      """
+    sqlContext.sql(
+      s"""
         |with weburl as (
         |   select collecttype, dt, engine, filedt, I_ID_D,source_app_name,dest_ip,dest_port,concat_ws('|',collect_set(url)) as url
-        |   from webrequest
+        |   from webrequests
         |   group by collecttype, dt, engine, filedt, I_ID_D,source_app_name,dest_ip,dest_port
         |)
-        |select c.*,w.url
+        |select c.*, w.url, $jobidcur as jobid, $jobidorig as jobidorig
         |from connections c
         |left outer join weburl w on (
         |  c.collecttype=w.collecttype
@@ -134,9 +111,8 @@ class Pipeline3To4(sqlContext: SQLContext) extends Pipeline {
         |  and c.dest_ip=w.dest_ip
         |)
       """.stripMargin)
-
-    dfres.withColumn("jobid", lit(jobidcur)).withColumn("jobidorig", lit(jobidorig))
-      .write.mode(SaveMode.Append).partitionBy("collecttype", "dt", "engine", "filedt", "jobidorig", "jobid").parquet(dirout)
+      .write.mode(SaveMode.Append)
+      .partitionBy("collecttype", "dt", "engine", "filedt", "jobidorig", "jobid").parquet(dirout)
   }
 
   def buildIpLookupTable(): DataFrame = {
@@ -145,7 +121,7 @@ class Pipeline3To4(sqlContext: SQLContext) extends Pipeline {
     broadcast(sqlContext.sql("""select mdm_ip, concat_ws('_',collect_set(mdm_loc_code)) as site from ip group by mdm_ip"""))
   }
 
-  def SiteResolutionFromIP(df: DataFrame): DataFrame = {
+  def resolveSite(df: DataFrame): DataFrame = {
 
     //Read MDM repository file
     val dfIP = buildIpLookupTable()
@@ -159,7 +135,7 @@ class Pipeline3To4(sqlContext: SQLContext) extends Pipeline {
         ++ List(formatSite($"sourceIP.site").as("source_site"),formatSite($"destIP.site").as("dest_site"))):_*)
   }
 
-  def SiteAndSectorResolutionFromI_ID(df: DataFrame): DataFrame = {
+  def resolveSector(df: DataFrame): DataFrame = {
     val dfI_ID = repo.readI_ID().select("I_ID","Sector","SiteCode")
 
     //join for resolution
@@ -168,12 +144,12 @@ class Pipeline3To4(sqlContext: SQLContext) extends Pipeline {
         ++ List($"dfID.Sector".as("source_sector"),formatSite($"dfID.SideCode").as("source_I_ID_site"))):_*)
   }
 
-  def AIPResolution(df: DataFrame): DataFrame = {
+  def resolveAIP(df: DataFrame): DataFrame = {
 
     val dfAIP = broadcast(repo.readAIP().persist(StorageLevel.MEMORY_AND_DISK))
 
-    df.join(dfAIP.as("destAip"), $"df.dest_ip" === $"destAip.aip_server_ip", "left_outer")
-      .join(dfAIP.as("sourceAip"), $"df.source_ip" === $"sourceAip.aip_server_ip", "left_outer")
+    df.join(dfAIP.as("destAip"), $"dest_ip" === $"destAip.aip_server_ip", "left_outer")
+      .join(dfAIP.as("sourceAip"), $"source_ip" === $"sourceAip.aip_server_ip", "left_outer")
       .select((df.columns.toList.map(df.col) ++
         List($"destAip.aip_server_adminby" as "dest_aip_server_adminby",
         $"destAip.aip_server_function" as "dest_aip_server_function",
