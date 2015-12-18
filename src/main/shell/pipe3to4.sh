e015364068a9326c8f441396fc40743855ba27c7
Guillaume Pinot <guillaume.pinot.fr@gmail.com>
2015-12-18 12:00:23 +0100

    Add begin and end date filtering (as name of 1st and 2nd file of inputFiles)

--- src/main/shell/pipe3to4.sh
+++ src/main/shell/pipe3to4.sh
@@ -5,11 +5,12 @@ CONF=/home/hadoop/conf/pipe3to4.conf
 submitArg=""
 DRYRUN=""
 VERBOSE=0
+DATERANGE=0
 
 LOGERR=$(mktemp)
 echo "error log in $LOGERR"
 
-usage="$0 [-n|--dry-run] [-v|--verbose] [submitArg]"
+usage="$0 [-n|--dry-run] [-v|--verbose] [-d|--date-range begindate endate] [submitArg]"
 while [[ $# > 0 ]]
 do
 key="$1"
@@ -25,6 +26,13 @@ case $key in
     -v|--verbose)
     VERBOSE=1
     ;;
+    -d|--date-range)
+    DATERANGE=1
+    begindate=$2
+    enddate=$3
+    shift
+    shift
+    ;;
     *)
     submitArg="$*"
     break
@@ -33,6 +41,17 @@ esac
 shift # past argument or value
 done
 
-echo "vide" | $SUBMIT -c $CONF $DRYRUN $submitArg 2>>$LOGERR
+tmpfile=$(mktemp)
+
+if [[ $DATERANGE -eq 1 ]]
+then
+    echo "$begindate" > $tmpfile
+    echo "$enddate" >> $tmpfile
+else
+    echo "nothing" > $tmpfile
+fi
+
+echo $tmpfile | $SUBMIT -c $CONF $DRYRUN $submitArg 2>>$LOGERR
 ret=$?
 echo "$0 : submit return : $ret"
+rm $tmpfile
