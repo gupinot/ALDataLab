4b9b96b3876661f828207f54a56a0da2cef34e90
Guillaume Pinot <guillaume.pinot.fr@gmail.com>
2015-12-21 22:39:26 +0100

    Add shell for dist-cp between s3 and hdfs

---
+++ src/main/shell/syncHdfsS3.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+CONF=/home/hadoop/conf/syncHdfsS3.conf
+
+dirdatas3=$(cat $CONF | egrep '^shell\.dirdatas3' | awk '{print $2}')
+dirdatahdfs=$(cat $CONF | egrep '^shell\.dirdatahdfs' | awk '{print $2}')
+FILEPATTERN=".*"
+DRYRUN=""
+VERBOSE=0
+method="fromS3"
+
+LOGERR=$(mktemp)
+echo "error log in $LOGERR"
+
+usage="$0 [-n|--dry-run] [-v|--verbose] fromS3|toS3"
+while [[ $# > 0 ]]
+do
+key="$1"
+
+case $key in
+    -h|--help)
+    echo "$usage"
+    exit 0
+    ;;
+    -n|--dry-run)
+    DRYRUN="-n"
+    ;;
+    -v|--verbose)
+    VERBOSE=1
+    ;;
+    fromS3|toS3)
+    method="$1"
+    break
+    ;;
+    *)
+    echo "argument not allowed"
+    exit 1
+    ;;
+esac
+shift # past argument or value
+done
+
+case $method in
+    fromS3)
+        s3-dist-cp --src=$dirdatas3 --dest=$dirdatahdfs
+    ;;
+    toS3)
+        s3-dist-cp --src=$dirdatahdfs --dest=$dirdatas3
+    ;;
+esac
+
