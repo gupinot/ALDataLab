ad663f70ab9211bcf205ba207ccd0f9f49a97152
gupinot <datalab@sarma10012.ad.sys>
2016-01-18 16:24:24 +0100

    update

---
+++ collect/serverconsumption/conf/conf.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+ROOTDIR=/datalab2/home/datalab/ALDataLab/collect/serverconsumption
+SERVERLST=$ROOTDIR/conf/server.lst
+URL="http://iww.dcs.itssc.alstom.com/nrtmd/streamsdump/server"
+dirserverxls="/datalab3/DATA/SERVER"
+
+dirin="/datalab3/DATA/SERVER"
+export dirdone="/datalab3/DATA/SERVER/done"; [[ -d $dirdone ]] || mkdir -p $dirdone
+dirserverxlstrash="/datalab3/DATA/SERVER/trash"; [[ -d $dirserverxlstrash ]] || mkdir -p $dirserverxlstrash
+export dirserverjson="/datalab3/DATA/SERVER-JSON"; [[ -d $dirserverjson ]] || mkdir -p $dirserverjson
+export dirserverjsonsent="$dirserverjson/sent"; [[ -d ${dirserverjsonsent} ]] || mkdir -p ${dirserverjsonsent}
+export convert_py=$ROOTDIR/../../infra/convert.py
+export dirnas="admin@10.249.49.99:/volume1/homes/datalab/serverusage"
