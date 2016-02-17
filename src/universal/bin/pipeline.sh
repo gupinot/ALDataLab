72d8336b3ae6d05e56e35f498e454964224541f4
Guillaume Pinot <guillaume.pinot.fr@gmail.com>
2016-02-17 16:36:47 +0100

    add serverusage in pipeline

--- src/universal/bin/pipeline.sh
+++ src/universal/bin/pipeline.sh
@@ -12,6 +12,7 @@ $HOME/pipeline/bin/pipe2to3.sh &&\
 $HOME/pipeline/bin/pipe3to4.sh &&\
 $HOME/pipeline/bin/pipe4to5.sh &&\
 $HOME/pipeline/bin/serverusage.sh &&\
+$HOME/pipeline/bin/webapp.sh &&\
 $HOME/pipeline/bin/syncHdfsS3.sh toS3; ret=$?
 
 echo "$(date +"%Y/%m/%d-%H:%M:%S") - $0 : end with exit code : $ret"
