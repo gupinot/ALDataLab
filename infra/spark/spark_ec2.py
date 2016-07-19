649e155ee8e21f13ff4c13f1d57435e61afcabc0
Raphael Luta <raphael.luta@aptiwan.com>
2016-07-19 14:43:35 +0200

    update default dependencies to spark 1.6.2 & zeppelin 0.6.0 & pipeline 1.3.2

--- infra/spark/spark_ec2.py
+++ infra/spark/spark_ec2.py
@@ -51,56 +51,36 @@ else:
     raw_input = input
     xrange = range
 
-SPARK_EC2_VERSION = "1.6.0"
+SPARK_EC2_VERSION = "1.6.2"
 SPARK_EC2_DIR = os.path.dirname(os.path.realpath(__file__))
 
 VALID_SPARK_VERSIONS = set([
-    "0.7.3",
-    "0.8.0",
-    "0.8.1",
-    "0.9.0",
-    "0.9.1",
-    "0.9.2",
-    "1.0.0",
-    "1.0.1",
-    "1.0.2",
-    "1.1.0",
-    "1.1.1",
-    "1.2.0",
-    "1.2.1",
-    "1.3.0",
-    "1.3.1",
     "1.4.0",
     "1.4.1",
     "1.5.0",
     "1.5.1",
     "1.5.2",
     "1.6.0",
+    "1.6.2"
 ])
 
 SPARK_ZEPPELIN_MAP = {
-    "1.6.0": "0.6.0-incubating-SNAPSHOT"
+    "1.6.0": "0.6.0-incubating-SNAPSHOT",
+    "1.6.2": "0.6.0"
 }
 
 SPARK_TACHYON_MAP = {
-    "1.0.0": "0.4.1",
-    "1.0.1": "0.4.1",
-    "1.0.2": "0.4.1",
-    "1.1.0": "0.5.0",
-    "1.1.1": "0.5.0",
-    "1.2.0": "0.5.0",
-    "1.2.1": "0.5.0",
-    "1.3.0": "0.5.0",
-    "1.3.1": "0.5.0",
     "1.4.0": "0.6.4",
     "1.4.1": "0.6.4",
     "1.5.0": "0.7.1",
     "1.5.1": "0.7.1",
     "1.5.2": "0.7.1",
     "1.6.0": "0.8.2",
+    "1.6.2": "0.8.2"
 }
 
 DEFAULT_SPARK_VERSION = SPARK_EC2_VERSION
+DEFAULT_PIPELINE_VERSION = "1.3.2"
 DEFAULT_SPARK_GITHUB_REPO = "https://github.com/apache/spark"
 
 # Default location to get the spark-ec2 scripts (and ami-list) from
@@ -299,7 +279,7 @@ def parse_args():
         "--pipeline-bucket", default="gedatalab",
         help="the s3 bucket name to use for pipeline binaries")
     parser.add_option(
-        "--pipeline-version", default="1.3.1",
+        "--pipeline-version", default=DEFAULT_PIPELINE_VERSION,
         help="the version of pipeline to use")
     parser.add_option(
         "--ganglia", action="store_true", default=False,
