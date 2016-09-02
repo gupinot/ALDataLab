061aaa18e6e880d6efce4eae81ec5d9f354a77ad
gupinot <datalab@sarma10012.ad.sys>
2016-09-02 11:21:47 +0200

    add oracle log

--- collect/oracle/bin/main.sh
+++ collect/oracle/bin/main.sh
@@ -15,14 +15,15 @@ done
 CURDIR=$PWD
 cd $LOCAL_ORACLE_LOG_DIR_IN
 
-PERLCOMMAND1=" s/([^ ]+)\s(\d\d:\d\d:\d\d)\s[^ ]+\s\(CONNECT_DATA=.*(SID|SERVICE_NAME)=([^\)]*)\).*PROGRAM=([^\)]*)\).*HOST=([^\)]*)\).*USER=([^\)]*)\).*\s.*PORT=(\d*).*establish\s[^ ]+\s([^ ]+)\s.*/\1;\2;\4;\5;\6;\7;\8;\9/"
-PERLCOMMAND2="s/([^ ]+)\s(\d\d:\d\d:\d\d)\s[^ ]+\s\(CONNECT_DATA=.*PROGRAM=([^\)]*)\).*HOST=([^\)]+)\).*USER=([^\)]*)\).*(SID|SERVICE_NAME|sid)=([^\)]+)\).*\s.*PORT=(\d+).*establish\s[^ ]+\s([^ ]+)\s.*/\1;\2;\7;\3;\4;\5;\8;\9/"
 
 for fic in $(ls *.zip)
 do
 	echo "fic : $fic"
+	hostname=$(echo $fic | cut -d_ -f2)
 	ficnoext=$(basename $fic .zip).log
-	echo "date;time;sid;program;host;user;port;instance" >  $LOCAL_ORACLE_LOG_DIR_ENCODED/${ficnoext}
+	PERLCOMMAND1="s/([^ ]+)\s(\d\d:\d\d:\d\d)\s[^ ]+\s\(CONNECT_DATA=.*(SID|SERVICE_NAME)=([^\)]*)\).*PROGRAM=([^\)]*)\).*HOST=([^\)]*)\).*USER=([^\)]*)\).*\s.*HOST=([^\)]*)\).*\(PORT=(\d*).*establish\s[^ ]+\s([^ ]+)\s.*/\1;\2;${hostname};\4;\5;\6;\8;\7;\9;\$10/"
+	PERLCOMMAND2="s/([^ ]+)\s(\d\d:\d\d:\d\d)\s[^ ]+\s\(CONNECT_DATA=.*PROGRAM=([^\)]*)\).*HOST=([^\)]+)\).*USER=([^\)]*)\).*(SID|SERVICE_NAME|sid)=([^\)]+)\).*\s.*HOST=([^\)]*)\).*\(PORT=(\d+).*establish\s[^ ]+\s([^ ]+)\s.*/\1;\2;${hostname};\7;\3;\4;\5;\9;\$10/"
+	echo "date;time;host_name;host_sid;source_program;source_host_name;source_host_ip;source_user;host_port;host_instance" >  $LOCAL_ORACLE_LOG_DIR_ENCODED/${ficnoext}
 	unzip -c $fic | grep CONNECT_DATA | grep establish | perl -ne "print if $PERLCOMMAND1" >> $LOCAL_ORACLE_LOG_DIR_ENCODED/${ficnoext} &&\
 	unzip -c $fic | grep CONNECT_DATA | grep establish | perl -ne "print if not $PERLCOMMAND1" | perl -ne "print if $PERLCOMMAND2" >>  $LOCAL_ORACLE_LOG_DIR_ENCODED/${ficnoext} &&\
 	gzip  $LOCAL_ORACLE_LOG_DIR_ENCODED/${ficnoext}
@@ -30,4 +31,4 @@ do
 	mv $fic $LOCAL_ORACLE_LOG_DIR_DONE 
 done
 
-
+cd $CURDIR
