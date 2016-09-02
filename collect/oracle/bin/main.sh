4d80eeb9bcf1dc80c0442a815a9ea13f4081bfe9
gupinot <datalab@sarma10012.ad.sys>
2016-09-02 18:03:55 +0200

    fix convert.py path

--- collect/oracle/bin/main.sh
+++ collect/oracle/bin/main.sh
@@ -22,11 +22,11 @@ do
 	hostname=$(echo $fic | cut -d_ -f2)
 	ficnoext=$(basename $fic .zip).log
 	PERLCOMMAND1="s/([^ ]+)\s(\d\d:\d\d:\d\d)\s[^ ]+\s\(CONNECT_DATA=.*(SID|SERVICE_NAME)=([^\)]*)\).*PROGRAM=([^\)]*)\).*HOST=([^\)]*)\).*USER=([^\)]*)\).*\s.*HOST=([^\)]*)\).*\(PORT=(\d*).*establish\s[^ ]+\s([^ ]+)\s.*/\1;\2;${hostname};\4;\5;\6;\8;\7;\9;\$10/"
-	PERLCOMMAND2="s/([^ ]+)\s(\d\d:\d\d:\d\d)\s[^ ]+\s\(CONNECT_DATA=.*PROGRAM=([^\)]*)\).*HOST=([^\)]+)\).*USER=([^\)]*)\).*(SID|SERVICE_NAME|sid)=([^\)]+)\).*\s.*HOST=([^\)]*)\).*\(PORT=(\d+).*establish\s[^ ]+\s([^ ]+)\s.*/\1;\2;${hostname};\7;\3;\4;\5;\9;\$10/"
+	PERLCOMMAND2="s/([^ ]+)\s(\d\d:\d\d:\d\d)\s[^ ]+\s\(CONNECT_DATA=.*PROGRAM=([^\)]*)\).*HOST=([^\)]+)\).*USER=([^\)]*)\).*(SID|SERVICE_NAME|sid)=([^\)]+)\).*\s.*HOST=([^\)]*)\).*\(PORT=(\d+).*establish\s[^ ]+\s([^ ]+)\s.*/\1;\2;${hostname};\7;\3;\4;\8;\5;\9;\$10/"
 	echo "date;time;host_name;host_sid;source_program;source_host_name;source_host_ip;source_user;host_port;host_instance" >  $LOCAL_ORACLE_LOG_DIR_ENCODED/${ficnoext}
 	unzip -c $fic | grep CONNECT_DATA | grep establish | perl -ne "print if $PERLCOMMAND1" >> $LOCAL_ORACLE_LOG_DIR_ENCODED/${ficnoext} &&\
 	unzip -c $fic | grep CONNECT_DATA | grep establish | perl -ne "print if not $PERLCOMMAND1" | perl -ne "print if $PERLCOMMAND2" >>  $LOCAL_ORACLE_LOG_DIR_ENCODED/${ficnoext} &&\
-	gzip  $LOCAL_ORACLE_LOG_DIR_ENCODED/${ficnoext}
+	gzip  $LOCAL_ORACLE_LOG_DIR_ENCODED/${ficnoext} && chmod 666 $LOCAL_ORACLE_LOG_DIR_ENCODED/${ficnoext}.gz
 	unzip -c $fic | grep CONNECT_DATA | grep establish | perl -ne "print if not $PERLCOMMAND1" | perl -ne "print if not $PERLCOMMAND2" > $LOCAL_ORACLE_LOG_DIR_TRASH/${ficnoext} &&\
 	mv $fic $LOCAL_ORACLE_LOG_DIR_DONE 
 done
