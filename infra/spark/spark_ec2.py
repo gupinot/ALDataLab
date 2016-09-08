791d470fecb806a361f5892a3beaddbecc67538f
Guillaume Pinot <guillaume.pinot.fr@gmail.com>
2016-09-08 09:06:23 +0200

    patch issue on modules value

--- infra/spark/spark_ec2.py
+++ infra/spark/spark_ec2.py
@@ -879,7 +879,7 @@ def setup_cluster(conn, master_nodes, slave_nodes, opts, deploy_ssh_key,cluster_
             ssh_write(slave_address, opts, ['tar', 'x'], dot_ssh_tar)
 
     modules = ['spark', 'ephemeral-hdfs', 'persistent-hdfs',
-               'mapreduce', 'spark-standalone', 'tachyon', 'rstudio','mysql'
+               'mapreduce', 'spark-standalone', 'tachyon', 'rstudio','mysql',
                'zeppelin','pipeline','s3proxy']
 
     if opts.hadoop_major_version == "1":
