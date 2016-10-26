b0ce60f17e96e07626cbbccc9712fe9f17575456
Nexthink <nexthink@sarma10012.ad.sys>
2016-10-26 17:10:15 +0200

    update

--- collect/nexthink/2/bin/SendMergedSplited.sh
+++ collect/nexthink/2/bin/SendMergedSplited.sh
@@ -19,14 +19,16 @@ for var in connection webrequest execution
 do
   for fic in $(ls ${MERGEDSPLITED}/${var}_*.gz)
   do
+    echo "$(date +"%Y/%m/%d-%H:%M:%S") - $0 : compute $fic"
     if lsof $fic; then 
-      #file being written
+      echo "$(date +"%Y/%m/%d-%H:%M:%S") - $0 : file being written"
       continue
     fi
     echo "$(date +"%Y/%m/%d-%H:%M:%S") - $0 : CMD : aws s3 cp $fic ${S3IN}/${var}/$(basename ${fic})"
     aws s3 cp $fic ${S3IN}/${var}/$(basename ${fic}); ret=$?
     if [[ $ret -eq 0 ]]
     then
+      echo "$(date +"%Y/%m/%d-%H:%M:%S") - $0 : mv file in done dir"
       mv $fic ${DONEMERGEDSPLITED}/.
     else
       echo "$(date +"%Y/%m/%d-%H:%M:%S") - $0 : CMD : aws s3 cp $fic ${S3IN}/${var}/$(basename ${fic}) : KO" >&2
